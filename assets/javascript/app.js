88757388e26c5ee083224010157caf34ab0bb9cb
Sheena Brevig <sheena.brevig@gmail.com>
2019-06-20 14:36:50 -0700

    on click radio button working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,96 +10,116 @@
 //append the questions into html
 
 
-//put correct answers in an answers array
-
+//have correct answers as object in our questionsArr 
 
 //if userChoice matches a value in the correct answers array( >= 0 ) then userScore++
+
 //if answer = -1 then incorrect answers++ 
 //if no answer selected, then display that number as Unanswered. 
 //want to store questions 
 
-// index[i].A[];
+// $("#start-button").on("click", start);
+
+// setTimeout(timeUp, 1000 * 30);
+
+
+// function start() {
+
+//     var timer = $("#questions-page").html("00:30");
+//     console.log(timer);
+//     //  TODO: Use setInterval to start the count here and set the clock to running.
+//     if (!clockRunning) {
+//       intervalId = setInterval(count, 1000);
+//       clockRunning = true;
+//     }  
+//   }
+
+
+// function timeUp() {
+//     $("#time-left").append("<h2>Time's Up!</h2>");
+//     console.log("time is up");
+// }
+
+// function reset() {
+//     time = 0;
+//     lap = 1;
+//     $("#laps").empty();
+  
+//     //  TODO: Change the "display" div to "00:00."
+//     $("#display").html("00:30")
+  
+//   }  
+
+
 
 var questionsArr = [
     {
         Q: "Q: How many Americans experience some form of mental illness?",
         A: ["1 in 5", "1 in 8", "1 in 10"],
-        CorrectAnswer: "1 of 5"
+        CorrectAnswer: 0
     },
 
     {
         Q: "Q: About how many people commit suicide every year?",
         A: ["50000", "800000", "10000"],
-        CorrectAnswer: "800000"
+        CorrectAnswer: 1
     },
 
     {
         Q: "Q: How many people say they have experienced mental health stigma?",
         A: ["1 in 10", "3 in 4", "2 in 7"],
-        CorrectAnswer: "3 in 4"
+        CorrectAnswer: 1
     },
 
     {
         Q: "Q: What is the leading cause of disability worldwide and is a major contributor to the global burden of disease?",
         A: [" Schizophrenia", "Anxiety", "Depression"],
-        CorrectAnswer: "Depression"
+        CorrectAnswer: 2
     }
 ];
 
 
 
-questionsArr.forEach(function (question) {
-
+questionsArr.forEach(function (question, questionsIndex) {
+    
     var col = $('<div class="col-6">');
     var card = $('<div class="card">');
     var header = $('<div class="card-header">').text(question.Q);
-
+    var name = questionsIndex;
     col.append(card);
     card.append(header);
 
-    question.A.forEach(function (answers) {
+    question.A.forEach(function (answers,answersIndex) {
         var span = $('<span>');
-        var radioBtn = $('<input type="radio" name="rbtnCount" />');
+        var radioBtn = $('<input  class="radio" type="radio" name="' + questionsIndex + '" index="'+ answersIndex + '"/>');
+        
         span.append(radioBtn,answers);
        
         card.append(span);
+
         console.log(answers);
     });
 
+        
+
     // card.append(radioBtn);
     $(".row").append(col);
+});
 
+$(".radio").on("click", function(){
+    var questionsIndex = $(this).attr('name');
+    var answersIndex = $(this).attr('index');
 
-
-
-    // answerOptions.append(answersSection);
-    // answersSection.append(card);
-    // radioBtn.appendTo(answerOptions);
-
-    // var  <ul class="list-group list-group-flush">
-    // console.log(question.Q);
-    // console.log(question.A);
-    // console.log(question.CorrectAnswer);
-
+    console.log(questionsIndex,answersIndex);
+    // does answersIndex = correct answer value
+    //if yes then ++userScore
 
 });
 
-//need radio button. 
-//in li
-//li in  ul 
-// ul in in card
 
-    // var answersSection = $('<ul class="list-group list-group-flush">');
-    // var answerOptions = $('<li class="list-group-item "></li>');
-    // var radioBtn = $('<input type="radio" name="rbtnCount" />');
+//in div class= "final-page"
 
-    // answerOptions.append(answersSection);
-    // answersSection.append(card);
-    // radioBtn.appendTo(answerOptions);
-
-
-    // + question.A[i] + 
-    // var header = $('<div class="card-header">').text(question.Q);
+var userScore;
 
 
 
@@ -111,17 +131,3 @@ questionsArr.forEach(function (question) {
 
 
 
-
-// for (let i=0; i< questionsArr.length; i++) {
-//     console.log[i];
-//     console.log(questionsArr[i].A);
-
-//     let currentAnswers = questionsArr[i].A
-//     for (let j=0; j < currentAnswers.length; j++){
-//         console.log(currentAnswers[j]);
-//     }
-// }
-
-// forEach ??
-
-// read up on forEach
